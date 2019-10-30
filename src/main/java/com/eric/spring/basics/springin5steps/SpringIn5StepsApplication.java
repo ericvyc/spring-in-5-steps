0d2a9f5be222adef114b0fc183e74be10f2e13c7
ericvyc <ericvyc@debian>
2019-10-30 19:29:55 -0400

    Autowired, Component and Sort Alg

--- src/main/java/com/eric/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/eric/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,18 +2,21 @@ package com.eric.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
-		binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
-		System.out.println(result);
-		
-		
-		SpringApplication.run(SpringIn5StepsApplication.class, args);
+
+//		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+//		binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+
+//		Application Context
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
 	}
 
 }
