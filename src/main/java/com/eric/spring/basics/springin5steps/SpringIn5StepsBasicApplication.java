891d504e452a29f981f63e93639350cc667fa00f
ericvyc <ericvyc@debian>
2019-11-01 13:35:47 -0400

    Logger and autowiring sub components by scope

--- src/main/java/com/eric/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/eric/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,13 +1,17 @@
 package com.eric.spring.basics.springin5steps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.eric.spring.basics.springin5steps.basic.BinarySearchImpl;
+import com.eric.spring.basics.springin5steps.scope.PersonDAO;
 
 @SpringBootApplication
 public class SpringIn5StepsBasicApplication {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsBasicApplication.class);
 
 	public static void main(String[] args) {
 
@@ -17,14 +21,14 @@ public class SpringIn5StepsBasicApplication {
 //		Application Context
 		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
 
-		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
-
-		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-
-		System.out.println(binarySearch);
-		System.out.println(binarySearch1);
-		System.out.println(result);
+		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
+		PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
+		
+		LOGGER.info("{}", personDAO);
+		LOGGER.info("{}", personDAO.getJdbcConnection());
+		
+		LOGGER.info("{}", personDAO2);
+		LOGGER.info("{}", personDAO2.getJdbcConnection());
 	}
 
 }
